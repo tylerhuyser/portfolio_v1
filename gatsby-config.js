3c97b8ebcf8d8ac850b3a45a4c493bfb57c1e327
Tyler Huyser <tyler.huyser@gmail.com>
2020-12-26 12:54:38 -0500

    updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Tyler Huyser',
     description:
-      'Brittany Chiang is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Tyler Huyser is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    siteUrl: 'https://tylerhuyser.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@tylerhuyser',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
