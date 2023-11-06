db596f087a87650626c9237049f9390fcfa67936
Tyler Huyser <tyler.huyser@gmail.com>
2023-11-06 16:26:24 -0500

    updated graphql syntax for sort for Archive.

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -221,7 +221,7 @@ export const pageQuery = graphql`
   {
     allMarkdownRemark(
       filter: { fileAbsolutePath: { regex: "/projects/" } }
-      sort: { fields: [frontmatter___date], order: DESC }
+      sort: { frontmatter: { date: DESC } }
     ) {
       edges {
         node {
